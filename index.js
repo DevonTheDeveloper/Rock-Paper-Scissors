6d52cd4d10a892499dfa4af46da5d5ee2316d963
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-02-05 16:04:28 +0100

    Restart project

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const scissorsButton = document.createElement("button", "#scissors");
 
 let computerScore = 0;
 let playerScore = 0;
-let playerSelection = "";
+let playerSelection = undefined;
 let computerSelection = getComputerChoice();
-const choices = ["rock", "paper", "scissors"];
 
 rockButton.textContent = "ROCK";
 rockButton.setAttribute("id", "rock");
@@ -21,92 +20,12 @@ container.appendChild(rockButton);
 container.appendChild(paperButton);
 container.appendChild(scissorsButton);
 
-// ↓ RANDOMIZE COMPUTER CHOICE ↓
-
-function getComputerChoice(computerSelection) {
-  const choices = ["rock", "paper", "scissors"];
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors'];
   let numberPicker = Math.floor(Math.random() * choices.length);
   return choices[numberPicker];
-}
+};
 
-// ↓ ROUND RESULTS ↓
-
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection.toLowerCase() === computerSelection) {
-    return console.log(
-      "You tied, you both chose " +
-        computerSelection +
-        "\nComputer Score: " +
-        computerScore +
-        " Your Score: " +
-        playerScore
-    );
-  }
-
-  if (
-    (playerSelection.toLowerCase() === "rock" &&
-      computerSelection === "scissors") ||
-    (playerSelection.toLowerCase() === "paper" &&
-      computerSelection === "rock") ||
-    (playerSelection.toLowerCase() === "scissors" &&
-      computerSelection === "paper")
-  ) {
-    playerScore++;
-    return console.log(
-      "You won the round! " +
-        playerSelection.toLowerCase() +
-        " beats " +
-        computerSelection +
-        "\nComputer Score: " +
-        computerScore +
-        " Your Score: " +
-        playerScore
-    );
-  }
-
-  if (
-    (computerSelection === "rock" &&
-      playerSelection.toLowerCase() === "scissors") ||
-    (computerSelection === "paper" &&
-      playerSelection.toLowerCase() === "rock") ||
-    (computerSelection === "scissors" &&
-      playerSelection.toLowerCase() === "paper")
-  ) {
-    computerScore++;
-    return console.log(
-      "You lost the round. " +
-        computerSelection +
-        " beats " +
-        playerSelection.toLowerCase() +
-        "\nComputer Score: " +
-        computerScore +
-        " Your Score: " +
-        playerScore
-    );
-  }
-}
-
-// ↓ LETS THE GAME GO 5 ROUNDS ↓
-
-// for (let i = 1; i <= 5; i++) {
-//   playerSelection = prompt(
-//     `Round ${i}, Choose. Rock, Paper or Scissors?`
-//   ).toLowerCase();
-
-// ↓ REPORTS FINAL RESULTS ↓
-
-//   if (i === 5 && computerScore == playerScore) {
-//     console.log(`Game over! You tied.`);
-//   }
-
-//   if (i === 5 && computerScore > playerScore) {
-//     console.log(`Game over! You lost.`);
-//   }
-
-//   if (i === 5 && playerScore > computerScore) {
-//     console.log(`Game over! You won!`);
-//   }
-// }
 
 // User Interface (UI)
 container.addEventListener("click", function (event) {
@@ -117,13 +36,8 @@ container.addEventListener("click", function (event) {
   ) {
     playerSelection = event.target.getAttribute("id");
   }
+  for (let i = 0; i < 5;) {
+    playRound(playerSelection, computerSelection)
+      i++;
+  };
 });
-
-// ↓ VALIDATES INPUT ↓
-if (
-  playerSelection.toLowerCase() != "rock" &&
-  playerSelection.toLowerCase() != "paper" &&
-  playerSelection.toLowerCase() != "scissors"
-) {
-  console.log(`${playerSelection} is not a choice.`);
-}
